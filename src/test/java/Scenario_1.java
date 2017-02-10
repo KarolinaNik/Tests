a4cd8ddb48f82505626536af64a03935221fd37a
KarolinaNik <karolinanik@gmail.com>
2017-02-10 15:38:44 +0000

    Added class Scenario_1

--- src/test/java/Scenario_1.java
+++ src/test/java/Scenario_1.java
@@ -4,19 +4,34 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+//import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+//import static org.openqa.selenium.support.ui.ExpectedConditions.invisibilityOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 
 /**
  * Created by Alexandra on 27/01/2017.
+
+ Scenario 1 for:
+ 1. Login
+ 2. Navigate to Admin => Structure
+ 3. Add new Client (@client)
+ 4. Add new Division (@division)
+ 5. Add new Academy (@academy)
+ 6. Add new Activity (@activity) - not working
+
+ Post-conditions: clear db
+ delete from Divisions where Name = 'Test Division 10'
+ delete from Clients where Name = 'Test Client 10'
+
  */
 
 
@@ -41,7 +56,7 @@ public class Scenario_1 {
         switch (browser) {
             case "Chrome":
                 System.setProperty("webdriver.chrome.driver", "C:\\Tools\\selenium drivers\\chromedriver.exe");
-                ChromeOptions options = new ChromeOptions();                        //to start Chrome full screen
+                ChromeOptions options = new ChromeOptions();
                 options.addArguments("--start-maximized");
                 driver = new ChromeDriver(options);
                 break;
@@ -55,33 +70,31 @@ public class Scenario_1 {
                 break;
         }
 
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, 20);
+        System.out.println("------------- Scenario execution: ------------- ");
     }
 
     @Test
     public void Scenario_1() {
 
-        //Login
+//1. Login
         driver.navigate().to(home_url);
         wait.until(presenceOfElementLocated(By.id("UserName")));
-        // wait.until(titleIs("Log in"));
         driver.findElement(By.id("UserName")).sendKeys(user);
         driver.findElement(By.id("Password")).clear();
         driver.findElement(By.id("Password")).sendKeys(password);
         driver.findElement(By.cssSelector("button.btn.btn-success.btn-block")).click();  //class name = "btn btn-success btn-block", Webdriver problem with spaces inside class names
         wait.until(presenceOfElementLocated(By.cssSelector("span.hidden-xs.usersFullname")));
-        // driver.findElement(By.cssSelector("span.hidden-xs.usersFullname"));
         Assert.assertEquals(driver.findElement(By.cssSelector("span.hidden-xs.usersFullname")).getAttribute("innerText"), "Alexandra Ilianova ");
 
 
-        //Navigating to Admin => Structure
+//2. Navigating to Admin => Structure
         driver.findElement(By.partialLinkText("Admin")).click();
         wait.until(presenceOfElementLocated(By.partialLinkText("Structure")));
         driver.findElement(By.partialLinkText("Structure")).click();
         wait.until(titleIs("Admin | Structure"));
 
-
-        //Add new client
+//3. Add new client
         wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-add-white")));
         driver.findElement(By.cssSelector("li.icon-stylized-add-white")).click();
 
@@ -91,7 +104,12 @@ public class Scenario_1 {
         wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-circle-ok-white.StructureFormSubmit")));
         driver.findElement(By.cssSelector("li.icon-stylized-circle-ok-white.StructureFormSubmit")).click();
 
-        //Add new division
+        //wait for success message disappearing
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div.notifyjs-bootstrap-base.notifyjs-bootstrap-success")));
+        System.out.println("Added new client: " + client);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.notifyjs-bootstrap-base.notifyjs-bootstrap-success")));
+
+//4. Add new division
         wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-add-white")));
         driver.findElement(By.cssSelector("li.icon-stylized-add-white")).click();
 
@@ -100,10 +118,15 @@ public class Scenario_1 {
 
         wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-circle-ok-white.StructureFormSubmit")));
         driver.findElement(By.cssSelector("li.icon-stylized-circle-ok-white.StructureFormSubmit")).click();
-        //icon-stylized-circle-ok-white StructureFormSubmit
 
-        //Add new academy
-        wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-add-white")));
+        //wait for success message disappearing
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div.notifyjs-bootstrap-base.notifyjs-bootstrap-success")));
+        System.out.println("Added new division: " + division);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.notifyjs-bootstrap-base.notifyjs-bootstrap-success")));
+
+
+//5. Add new academy
+     /*   wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-add-white")));
         driver.findElement(By.cssSelector("li.icon-stylized-add-white")).click();
 
         wait.until(presenceOfElementLocated(By.cssSelector("input#Title.form-field-lg")));
@@ -112,9 +135,14 @@ public class Scenario_1 {
         wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-circle-ok-white.StructureFormSubmit")));
         driver.findElement(By.cssSelector("li.icon-stylized-circle-ok-white.StructureFormSubmit")).click();
 
+        //wait for success message disappearing
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div.notifyjs-bootstrap-base.notifyjs-bootstrap-success")));
+        System.out.println("Added new academy: " + academy);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.notifyjs-bootstrap-base.notifyjs-bootstrap-success")));
+*/
 
-        //Add new activity
-        wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-add-white")));
+//6. Add new activity
+/*        wait.until(presenceOfElementLocated(By.cssSelector("li.icon-stylized-add-white")));
         driver.findElement(By.cssSelector("li.icon-stylized-add-white")).click();
 
         wait.until(presenceOfElementLocated(By.cssSelector("input#Title.form-field")));
@@ -123,7 +151,7 @@ public class Scenario_1 {
         //driver.findElement(By.cssSelector("a.chosen-single")).sendKeys("Enable");
         //driver.findElement(By.cssSelector("a.chosen-single.chosen-default")).sendKeys("English (United States)");
         //driver.findElement(By.cssSelector("input#Ends.form-field-sm.datetimeinput.hasDatepicker.valid")).sendKeys("07/31/2017 00:00");
-
+*/
     }
 
     // Проверки:
